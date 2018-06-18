48420a05185e7f16e4a96999d32c69b28fafc740
Johan Vos <johan.vos@gluonhq.com>
2018-06-18 10:57:55 +0200

    make it work on mac and linux

--- topics/javafx3d/hellosphere/src/main/java/org/projavafx/javafx3d/hellosphere/SimpleSphere.java
+++ topics/javafx3d/hellosphere/src/main/java/org/projavafx/javafx3d/hellosphere/SimpleSphere.java
@@ -3,6 +3,7 @@ package org.projavafx.javafx3d.hellosphere;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.shape.Sphere;
 import javafx.stage.Stage;
 
@@ -10,8 +11,10 @@ public class SimpleSphere extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        Label l = new Label("Hello, JavaFX 3D");
+        l.setTranslateY(80);
         Sphere sphere = new Sphere(50);
-        Group root = new Group(sphere);
+        Group root = new Group(l, sphere);
         root.setTranslateX(320);
         root.setTranslateY(240);
         Scene scene = new Scene(root, 640, 480);
