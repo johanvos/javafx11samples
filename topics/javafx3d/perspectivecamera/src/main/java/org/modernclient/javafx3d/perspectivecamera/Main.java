8f6e9fcdfc3b071009b44a738b8f09eb6c4b9f64
Johan Vos <johan@lodgon.com>
2018-07-01 12:14:44 +0200

    simplify code

--- topics/javafx3d/perspectivecamera/src/main/java/org/modernclient/javafx3d/perspectivecamera/Main.java
+++ topics/javafx3d/perspectivecamera/src/main/java/org/modernclient/javafx3d/perspectivecamera/Main.java
@@ -5,6 +5,7 @@ import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.scene.Camera;
 import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
@@ -15,63 +16,50 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class Main extends Application {
-    private View view;
+
+    private final Rotate rotateX = new Rotate(-20, Rotate.X_AXIS);
+    private final Rotate rotateY = new Rotate(-20, Rotate.Y_AXIS);
+    private final Rotate rotateZ = new Rotate(-20, Rotate.Z_AXIS);
+    private final Translate translateZ = new Translate(0, 0, -100);
 
     @Override
     public void start(Stage stage) throws Exception {
-        view = new View();
+        Camera camera = createCamera();
+        Box box = new Box(10, 10, 10);
+        Group view = new Group(box, camera);
+        Scene scene = new Scene(view, 640, 480);
+        scene.setCamera(camera);
         stage.setTitle("PerspectiveCamera Example");
-        stage.setScene(view.scene);
+        stage.setScene(scene);
         stage.show();
-        view.animate();
+        animate();
+    }
+
+    private Camera createCamera() {
+        Camera answer = new PerspectiveCamera(true);
+        answer.getTransforms().addAll(rotateX, rotateY, rotateZ, translateZ);
+        return answer;
+    }
+
+    private void animate() {
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(0),
+                        new KeyValue(translateZ.zProperty(), -20),
+                        new KeyValue(rotateX.angleProperty(), 90),
+                        new KeyValue(rotateY.angleProperty(), 90),
+                        new KeyValue(rotateZ.angleProperty(), 90)),
+                new KeyFrame(Duration.seconds(5),
+                        new KeyValue(translateZ.zProperty(), -80),
+                        new KeyValue(rotateX.angleProperty(), -90),
+                        new KeyValue(rotateY.angleProperty(), -90),
+                        new KeyValue(rotateZ.angleProperty(), -90))
+        );
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
     }
 
     public static void main(String[] args) {
         launch(args);
     }
-    private static class View {
-        public Scene scene;
 
-        public Box box;
-        public PerspectiveCamera camera;
-
-        private final Rotate rotateX;
-        private final Rotate rotateY;
-        private final Rotate rotateZ;
-        private final Translate translateZ;
-
-        private View() {
-            box = new Box(10, 10, 10);
-            camera = new PerspectiveCamera(true);
-
-            rotateX = new Rotate(-20, Rotate.X_AXIS);
-            rotateY = new Rotate(-20, Rotate.Y_AXIS);
-            rotateZ = new Rotate(-20, Rotate.Z_AXIS);
-            translateZ = new Translate(0, 0, -100);
-
-            camera.getTransforms().addAll(rotateX, rotateY, rotateZ, translateZ);
-
-            Group group = new Group(box, camera);
-            scene = new Scene(group, 640, 480);
-            scene.setCamera(camera);
-        }
-
-        public void animate() {
-            Timeline timeline = new Timeline(
-                    new KeyFrame(Duration.seconds(0),
-                            new KeyValue(translateZ.zProperty(), -20),
-                            new KeyValue(rotateX.angleProperty(), 90),
-                            new KeyValue(rotateY.angleProperty(), 90),
-                            new KeyValue(rotateZ.angleProperty(), 90)),
-                    new KeyFrame(Duration.seconds(5),
-                            new KeyValue(translateZ.zProperty(), -80),
-                            new KeyValue(rotateX.angleProperty(), -90),
-                            new KeyValue(rotateY.angleProperty(), -90),
-                            new KeyValue(rotateZ.angleProperty(), -90))
-            );
-            timeline.setCycleCount(Animation.INDEFINITE);
-            timeline.play();
-        }
-    }
 }
-
